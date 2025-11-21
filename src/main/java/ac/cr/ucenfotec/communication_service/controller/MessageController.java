e9390eb45e9c67d637a3449e15161d68cf57e4bd
dherrerap <dherrerap@ucenfotec.ac.cr>
2025-11-21 00:22:42 -0600

    Add getMessagesByReceptor endpoint

--- src/main/java/ac/cr/ucenfotec/communication_service/controller/MessageController.java
+++ src/main/java/ac/cr/ucenfotec/communication_service/controller/MessageController.java
@@ -1,26 +1,45 @@
 package ac.cr.ucenfotec.communication_service.controller;
 
+import ac.cr.ucenfotec.communication_service.dto.SystemId;
 import ac.cr.ucenfotec.communication_service.model.MessageRequest;
 import ac.cr.ucenfotec.communication_service.model.MessageResponse;
 import ac.cr.ucenfotec.communication_service.service.MessageService;
+import ac.cr.ucenfotec.communication_service.service.QueueService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class MessageController {
 
     private final MessageService messageService;
+    private final QueueService queueService;
 
-    public MessageController(MessageService messageService) {
+    public MessageController(MessageService messageService, QueueService queueService) {
         this.messageService = messageService;
+        this.queueService = queueService;
     }
 
     @PostMapping("/mensaje")
     public ResponseEntity<MessageResponse> sendMessage(@Valid @RequestBody MessageRequest request) {
         MessageResponse response = messageService.processMessage(request);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/mensaje/{receptor}")
+    public ResponseEntity<List<MessageResponse>> getMessagesByReceptor(@PathVariable String receptor) {
+
+        SystemId receptorEnum;
+        try {
+            receptorEnum = SystemId.valueOf(receptor);
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(queueService.getMessagesFor(receptorEnum));
     }
 
 }
