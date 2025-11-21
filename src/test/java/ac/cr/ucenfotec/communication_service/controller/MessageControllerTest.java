adf21409877f0d2d8ba94369c149fa2fc04ffbde
dherrerap <dherrerap@ucenfotec.ac.cr>
2025-11-21 00:21:06 -0600

    Add TDD for getMessages

--- src/test/java/ac/cr/ucenfotec/communication_service/controller/MessageControllerTest.java
+++ src/test/java/ac/cr/ucenfotec/communication_service/controller/MessageControllerTest.java
@@ -1,6 +1,10 @@
 package ac.cr.ucenfotec.communication_service.controller;
 
+import ac.cr.ucenfotec.communication_service.dto.SystemId;
+import ac.cr.ucenfotec.communication_service.model.MessageResponse;
 import ac.cr.ucenfotec.communication_service.service.MessageService;
+import ac.cr.ucenfotec.communication_service.service.QueueService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -11,8 +15,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MessageController.class)
 class MessageControllerTest {
@@ -23,12 +33,20 @@ class MessageControllerTest {
     @Autowired
     private MessageService messageService;
 
+    @Autowired
+    private QueueService queueService;
+
     @TestConfiguration
     static class TestConfig {
 
         @Bean
         MessageService messageService() {
             return Mockito.mock(MessageService.class);
+        }
+
+        @Bean
+        QueueService queueService() {
+            return Mockito.mock(QueueService.class);
         }
     }
 
@@ -37,6 +55,7 @@ class MessageControllerTest {
     }
 
     private final String url = "/api/mensaje";
+    private final String GET_MESSAGES_URL = "/api/mensaje/";
 
     @Test
     void sendMessageSuccessfully() throws Exception {
@@ -156,4 +175,54 @@ class MessageControllerTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    void getMessagesSuccessfully() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        String receptor = "S02_REC";
+
+        MessageResponse mockResponse = new MessageResponse();
+        mockResponse.setId("123");
+        mockResponse.setEmisor(SystemId.S01_COM);
+        mockResponse.setReceptor(SystemId.S02_REC);
+        mockResponse.setMensaje(mapper.readTree("{\"a\":1}"));
+        mockResponse.setTimestamp(Instant.now());
+
+        when(queueService.getMessagesFor(SystemId.S02_REC))
+                .thenReturn(List.of(mockResponse));
+
+        mockMvc.perform(get(GET_MESSAGES_URL + receptor))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value("123"))
+                .andExpect(jsonPath("$[0].emisor").value("S01_COM"))
+                .andExpect(jsonPath("$[0].receptor").value("S02_REC"))
+                .andExpect(jsonPath("$[0].mensaje.a").value(1));
+    }
+
+    @Test
+    void getMessagesEmptyList() throws Exception {
+
+        Mockito.when(queueService.getMessagesFor(SystemId.S02_REC))
+                .thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get(GET_MESSAGES_URL + "S02_REC"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(0));
+    }
+
+    @Test
+    void getMessagesInvalidReceptor() throws Exception {
+        mockMvc.perform(get(GET_MESSAGES_URL + "INVALID_ID"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void getMessagesMissingReceptor() throws Exception {
+        mockMvc.perform(get(GET_MESSAGES_URL))
+                .andExpect(status().isNotFound());
+    }
+
 }
